a6cb3109042ef231098b4dbb5756be08ab49d3e9
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-01-09 17:20:16 +0100

    first commit

---
+++ a-wonderwp-autoloader.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  Autoload Wonderwp
+Plugin URI:   http://wonderwp.com/autoload
+Description:  Wonderwp Autoload mechanism
+Version:      1.0.0
+Author:       Wonderful
+Author URI:   http://digital.wonderful.fr
+License:      MIT License
+*/
+
+\WonderWp\Framework\Loader::getInstance();
