162cb70d141ad9243f4d46f5fcc46cefa66cdedf
Jeremy Desvaux <jeremy@jdmweb.com>
2025-02-04 12:29:53 +0100

    feat(loader): ability to load init files from bundles

--- autoload-wwp.php
+++ autoload-wwp.php
@@ -15,4 +15,23 @@ if(file_exists($composerAutoLoaderFile)){
     $loader = include_once($composerAutoLoaderFile);
 }
 
+// Parse loaded bundles to init them
+$wonderwpDir = ABSPATH . '/vendor/wonderwp';
+if (!is_dir($wonderwpDir) && defined('ROOT_DIR')) {
+    $wonderwpDir = ROOT_DIR . '/vendor/wonderwp';
+}
+
+if (is_dir($wonderwpDir)) {
+    $bundles = scandir($wonderwpDir);
+    foreach ($bundles as $bundle) {
+        if ($bundle !== '.' && $bundle !== '..' && is_dir($wonderwpDir . '/' . $bundle)) {
+            $initFile = $wonderwpDir . '/' . $bundle . '/init.php';
+            if (file_exists($initFile)) {
+                include_once($initFile);
+            }
+        }
+    }
+}
+
+// Then load the WonderWp loader
 \WonderWp\Bundle\Loader::getInstance();
